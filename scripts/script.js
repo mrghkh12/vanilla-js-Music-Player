f9ac1778f03b609e4a6995e0e8ee6753706c7cfc
mohammad reza <mrghkh12@gmail.com>
2024-07-18 18:12:13 +0330

    feat(script.js): coordination timeBar and song currentTime

--- scripts/script.js
+++ scripts/script.js
@@ -12,11 +12,7 @@ const nextSongBtn = $.querySelector('.nextSongBtn')
 const songName = $.querySelector('.songName')
 const artistName = $.querySelector('.artistName')
 
-
 const timeBar = $.querySelector('#timeBar')
-
-
-
 
 ctrlBtn.addEventListener('click' , () => {
     if(ctrlIcon.classList.contains('fa-pause')){
@@ -29,3 +25,24 @@ ctrlBtn.addEventListener('click' , () => {
         ctrlIcon.classList.remove('fa-play')
     }
 })
+
+songElem.addEventListener('loadedmetadata' , () => {
+    timeBar.max = Math.floor(songElem.duration);
+})
+let isOK = null
+timeBar.addEventListener('mousedown', (e)=>{
+    clearInterval(isOK)
+})
+timeBar.addEventListener('click', (e)=>{
+    songElem.currentTime = Math.round((e.offsetX/e.target.offsetWidth) * timeBar.max)
+    timeBar.value = songElem.currentTime;
+    ctrlIcon.classList.add('fa-pause')
+    ctrlIcon.classList.remove('fa-play')
+    songElem.play()
+})
+timeBar.addEventListener('mouseup', ()=>{
+    isOK = setInterval(()=>timeBar.value = songElem.currentTime , 1000)
+})
+if(songElem.play){
+    isOK = setInterval(()=>timeBar.value = songElem.currentTime , 1000)
+}
